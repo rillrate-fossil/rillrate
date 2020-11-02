6686394f6cef5c21f1995287290352016c724d78
overfloat <rillrate@gmail.com>
2020-11-02 22:14:40 +0300

    Add loop to the example

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,11 +1,15 @@
 use anyhow::Error;
 use rill::provider::ProviderCell;
+use std::thread;
+use std::time::Duration;
 
 static RILL: ProviderCell = ProviderCell::new(std::module_path!());
 
 fn main() -> Result<(), Error> {
     rill::install()?;
     rill::bind_all(&[&RILL]);
-    RILL.log("Data".into());
-    Ok(())
+    loop {
+        RILL.log("Data".into());
+        thread::sleep(Duration::from_millis(500));
+    }
 }
