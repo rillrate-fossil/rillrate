8f8edd8e48d780601de591ac4dcb862eb1f7f5d6
overfloat <rillrate@gmail.com>
2020-12-20 14:46:38 +0300

    Add heartbeat to the GraphiteExporter

--- src/exporters/graphite.rs
+++ src/exporters/graphite.rs
@@ -1,22 +1,30 @@
 use crate::actors::supervisor::RillSupervisor;
 use crate::exporters::BroadcastData;
+use crate::protocol::{Path, RillData};
 use anyhow::Error;
 use async_trait::async_trait;
 use futures::StreamExt;
 use meio::prelude::{
-    Actor, Address, Context, IdOf, Interaction, InteractionHandler, InterruptedBy, LiteTask,
-    StartedBy, StopReceiver, Task, TryConsumer,
+    task::{HeartBeat, Tick},
+    ActionHandler, Actor, Address, Context, Eliminated, IdOf, Interaction, InteractionHandler,
+    InterruptedBy, LiteTask, StartedBy, StopReceiver, Task, TryConsumer,
 };
+use std::collections::HashMap;
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::sync::broadcast;
 
 pub struct GraphiteExporter {
     rx: Option<broadcast::Receiver<Arc<BroadcastData>>>,
+    metrics: HashMap<Path, RillData>,
 }
 
 impl GraphiteExporter {
     pub fn new(receiver: broadcast::Receiver<Arc<BroadcastData>>) -> Self {
-        Self { rx: Some(receiver) }
+        Self {
+            rx: Some(receiver),
+            metrics: HashMap::new(),
+        }
     }
 }
 
@@ -36,6 +44,8 @@ impl StartedBy<RillSupervisor> for GraphiteExporter {
             .into_stream()
             .boxed();
         ctx.address().attach(rx);
+        let heartbeat = HeartBeat::new(Duration::from_millis(1_000), ctx.address().clone());
+        ctx.spawn_task(heartbeat, ());
         Ok(())
     }
 }
@@ -49,6 +59,28 @@ impl InterruptedBy<RillSupervisor> for GraphiteExporter {
 }
 
 #[async_trait]
+impl Eliminated<Task<HeartBeat>> for GraphiteExporter {
+    async fn handle(
+        &mut self,
+        _id: IdOf<Task<HeartBeat>>,
+        ctx: &mut Context<Self>,
+    ) -> Result<(), Error> {
+        ctx.shutdown();
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl ActionHandler<Tick> for GraphiteExporter {
+    async fn handle(&mut self, _: Tick, ctx: &mut Context<Self>) -> Result<(), Error> {
+        for (path, data) in self.metrics.drain() {
+            // TODO: Render lines and send them pickled
+        }
+        Ok(())
+    }
+}
+
+#[async_trait]
 impl TryConsumer<Arc<BroadcastData>> for GraphiteExporter {
     type Error = broadcast::RecvError;
 
@@ -57,7 +89,8 @@ impl TryConsumer<Arc<BroadcastData>> for GraphiteExporter {
         item: Arc<BroadcastData>,
         _ctx: &mut Context<Self>,
     ) -> Result<(), Error> {
-        todo!("send rendered line into a graphite connection");
+        self.metrics.insert(item.path.clone(), item.data.clone());
+        Ok(())
     }
 
     async fn error(&mut self, err: Self::Error, ctx: &mut Context<Self>) -> Result<(), Error> {
