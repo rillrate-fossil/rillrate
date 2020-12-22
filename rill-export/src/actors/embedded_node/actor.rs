547c322970bd0771d9af44152709cb9bcac2b7aa
overfloat <rillrate@gmail.com>
2020-12-22 15:02:08 +0300

    Add entrypoint to rill-export

--- rill-export/src/actors/embedded_node/actor.rs
+++ rill-export/src/actors/embedded_node/actor.rs
@@ -1,7 +1,22 @@
-use meio::prelude::Actor;
+use anyhow::Error;
+use async_trait::async_trait;
+use meio::prelude::{Actor, Context, StartedBy, System};
 
 pub struct EmbeddedNode {}
 
 impl Actor for EmbeddedNode {
     type GroupBy = ();
+}
+
+impl EmbeddedNode {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+#[async_trait]
+impl StartedBy<System> for EmbeddedNode {
+    async fn handle(&mut self, ctx: &mut Context<Self>) -> Result<(), Error> {
+        todo!();
+    }
 }
--- rill-export/src/lib.rs
+++ rill-export/src/lib.rs
@@ -1,7 +1,11 @@
 mod actors;
 
 use anyhow::Error;
+use std::thread;
 
 pub fn start_node() -> Result<(), Error> {
+    thread::Builder::new()
+        .name("rill-export".into())
+        .spawn(|| actors::runtime::entrypoint())?;
     Ok(())
 }
