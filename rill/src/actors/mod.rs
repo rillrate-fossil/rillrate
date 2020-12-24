183640c97830e1bdaeeda69c29ea78ceda8fd5f8
overfloat <rillrate@gmail.com>
2020-12-25 00:30:41 +0300

    Use thread module of meio

--- rill/src/actors/mod.rs
+++ rill/src/actors/mod.rs
@@ -1,3 +1,2 @@
-pub mod runtime;
 pub mod supervisor;
 mod worker;
--- rill/src/lib.rs
+++ rill/src/lib.rs
@@ -7,14 +7,14 @@ pub mod protocol;
 pub mod providers;
 mod state;
 
-use actors::runtime::term;
+use crate::actors::supervisor::RillSupervisor;
+use anyhow::Error;
 use protocol::EntryId;
 use state::{RillState, RILL_STATE};
-use std::thread;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum Error {
+pub enum RillError {
     #[error("alreary installed")]
     AlreadyInstalled,
     #[error("io error {0}")]
@@ -30,50 +30,17 @@ pub enum Error {
 }
 
 pub struct Rill {
-    sender: Option<term::Sender>,
+    scoped: meio::thread::ScopedRuntime,
 }
 
 impl Rill {
     pub fn install(name: impl Into<EntryId>) -> Result<Self, Error> {
-        let (term_tx, term_rx) = term::channel();
         let (rx, state) = RillState::create();
-        RILL_STATE.set(state).map_err(|_| Error::AlreadyInstalled)?;
-        let entry_id = name.into();
-        thread::Builder::new()
-            .name("rill".into())
-            .spawn(move || actors::runtime::entrypoint(entry_id, rx, term_rx))?;
-        Ok(Self {
-            sender: Some(term_tx),
-        })
-    }
-
-    /* TODO: I'm not sure the methods below are necessary. Keep it commented.
-    pub fn exempt(mut self) {
-        self.no_wait();
-        // And let it `Drop`.
-    }
-
-    /// Don't wait for the `Worker` termination on drop of this instance.
-    pub fn no_wait(&mut self) {
-        self.sender.take();
-    }
-    */
-
-    fn terminate(&mut self) {
-        if let Some(sender) = self.sender.take() {
-            if let Err(_) = sender.notifier_tx.send(()) {
-                log::error!("Can't send termination signal to the rill state.");
-                return;
-            }
-            if let Err(_) = sender.blocker.lock() {
-                log::error!("Can't wait for termination of the rill state.");
-            }
-        }
-    }
-}
-
-impl Drop for Rill {
-    fn drop(&mut self) {
-        self.terminate();
+        RILL_STATE
+            .set(state)
+            .map_err(|_| RillError::AlreadyInstalled)?;
+        let actor = RillSupervisor::new(name.into(), rx);
+        let scoped = meio::thread::spawn(actor)?;
+        Ok(Self { scoped })
     }
 }
