063725e88f6bfba41ef63eec4f5803b1681c0c36
overfloat <rillrate@gmail.com>
2020-12-28 23:43:23 +0300

    Read config file by Tuner

--- rill-export/src/actors/tuner/actor.rs
+++ rill-export/src/actors/tuner/actor.rs
@@ -1,7 +1,13 @@
 use crate::actors::embedded_node::EmbeddedNode;
 use anyhow::Error;
 use async_trait::async_trait;
-use meio::prelude::{Action, ActionHandler, Actor, Context, InterruptedBy, StartedBy};
+use meio::prelude::{
+    Action, ActionHandler, Actor, Context, IdOf, InterruptedBy, LiteTask, StartedBy,
+    TaskEliminated, TaskError,
+};
+use serde::{Deserialize, Serialize};
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 
 pub struct Tuner {}
 
@@ -18,7 +24,7 @@ impl Actor for Tuner {
 #[async_trait]
 impl StartedBy<EmbeddedNode> for Tuner {
     async fn handle(&mut self, ctx: &mut Context<Self>) -> Result<(), Error> {
-        ctx.address().act(Configure).await?;
+        ctx.spawn_task(ReadConfigFile, ());
         Ok(())
     }
 }
@@ -31,13 +37,43 @@ impl InterruptedBy<EmbeddedNode> for Tuner {
     }
 }
 
-struct Configure;
+#[async_trait]
+impl TaskEliminated<ReadConfigFile> for Tuner {
+    async fn handle(
+        &mut self,
+        _id: IdOf<ReadConfigFile>,
+        result: Result<Config, TaskError>,
+        ctx: &mut Context<Self>,
+    ) -> Result<(), Error> {
+        match result {
+            Ok(config) => for path_to_export in config.export {},
+            Err(err) => {
+                log::warn!(
+                    "Can't read config file. No special configuration parameters applied: {}",
+                    err
+                );
+            }
+        }
+        Ok(())
+    }
+}
 
-impl Action for Configure {}
+#[derive(Serialize, Deserialize)]
+struct Config {
+    export: Vec<String>,
+}
+
+struct ReadConfigFile;
 
 #[async_trait]
-impl ActionHandler<Configure> for Tuner {
-    async fn handle(&mut self, _: Configure, ctx: &mut Context<Self>) -> Result<(), Error> {
-        Ok(())
+impl LiteTask for ReadConfigFile {
+    type Output = Config;
+
+    async fn interruptable_routine(mut self) -> Result<Self::Output, Error> {
+        let mut file = File::open("rill.toml").await?;
+        let mut contents = Vec::new();
+        file.read_to_end(&mut contents).await?;
+        let config: Config = toml::from_slice(&contents)?;
+        Ok(config)
     }
 }
