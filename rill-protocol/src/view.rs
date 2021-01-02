581bc2455af9d11efc1d31e1dad7efab3079189e
overfloat <rillrate@gmail.com>
2021-01-02 22:31:02 +0300

    Add ViewProtocol

--- rill-protocol/src/view.rs
+++ rill-protocol/src/view.rs
@@ -1,1 +1,21 @@
+use crate::codec::JsonCodec;
+use crate::provider::{Envelope, Origin};
+use meio_protocol::Protocol;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub struct ViewProtocol;
+
+impl Protocol for ViewProtocol {
+    type ToServer = Envelope<Self, ViewRequest>;
+    type ToClient = Envelope<Self, ViewResponse>;
+    type Codec = JsonCodec;
+}
+
+impl Origin for ViewProtocol {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum ViewRequest {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum ViewResponse {}
