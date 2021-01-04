0778370f0a6cd5c0f326d46c4cc720e24334436c
overfloat <rillrate@gmail.com>
2021-01-04 11:32:50 +0300

    Implement TryConsumer for the ClientSession

--- rill-export/src/actors/client_session/actor.rs
+++ rill-export/src/actors/client_session/actor.rs
@@ -1,16 +1,18 @@
-use crate::actors::exporter::ExporterLinkForClient;
+use crate::actors::exporter::{ExportEvent, ExporterLinkForClient};
 use crate::actors::provider_session::ProviderSessionLink;
 use crate::actors::server::Server;
 use anyhow::Error;
 use async_trait::async_trait;
 use meio::prelude::{
     ActionHandler, Actor, Context, IdOf, InterruptedBy, StartedBy, TaskEliminated, TaskError,
+    TryConsumer,
 };
 use meio_connect::{
     server::{WsHandler, WsProcessor},
     TermReason, WsIncoming,
 };
 use rill_protocol::view::{ViewProtocol, ViewRequest, ViewResponse};
+use tokio::sync::broadcast;
 
 pub struct ClientSession {
     handler: WsHandler<ViewProtocol>,
@@ -38,6 +40,9 @@ impl StartedBy<Server> for ClientSession {
     async fn handle(&mut self, ctx: &mut Context<Self>) -> Result<(), Error> {
         let worker = self.handler.worker(ctx.address().clone());
         ctx.spawn_task(worker, ());
+        self.exporter
+            .grasp_export_stream(ctx.address().clone())
+            .await?;
         Ok(())
     }
 }
@@ -89,6 +94,34 @@ impl ActionHandler<WsIncoming<ViewRequest>> for ClientSession {
                 todo!();
             }
         }
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl TryConsumer<ExportEvent> for ClientSession {
+    type Error = broadcast::RecvError;
+
+    async fn handle(&mut self, event: ExportEvent, _ctx: &mut Context<Self>) -> Result<(), Error> {
+        match event {
+            ExportEvent::SetInfo { .. } => {}
+            ExportEvent::BroadcastData {
+                path,
+                data,
+                timestamp,
+            } => {
+                todo!("filter and forward");
+            }
+        }
+        Ok(())
+    }
+
+    async fn error(&mut self, err: Self::Error, ctx: &mut Context<Self>) -> Result<(), Error> {
+        log::error!(
+            "Broadcasting stream failed. Not possible to continue: {}",
+            err
+        );
+        ctx.shutdown();
         Ok(())
     }
 }
