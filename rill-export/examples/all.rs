90c3ae1e16e82a7080d89478f8ae60442c4fe589
overfloat <rillrate@gmail.com>
2021-01-08 17:26:13 +0300

    Add second counter to the example

--- rill-export/examples/all.rs
+++ rill-export/examples/all.rs
@@ -20,25 +20,29 @@ fn main() -> Result<(), Error> {
         r.store(false, Ordering::SeqCst);
     })?;
 
-    let counter = CounterProvider::new("my.counter".parse()?);
+    let counter_one = CounterProvider::new("my.counter.one".parse()?);
+    let counter_two = CounterProvider::new("my.counter.two".parse()?);
     let gauge = GaugeProvider::new("my.gauge".parse()?);
     let fast_gauge = GaugeProvider::new("my.gauge.fast".parse()?);
     let random_gauge = GaugeProvider::new("my.gauge.random".parse()?);
     let logger = LogProvider::new("my.direct.logs.trace".parse()?);
     while running.load(Ordering::SeqCst) {
         for x in 0..3 {
+            counter_two.inc(1.0, None);
             fast_gauge.set(x as f64, None);
             random_gauge.set(rand::random(), None);
             thread::sleep(Duration::from_millis(100));
         }
         gauge.set(1.0, None);
         for x in 0..7 {
+            counter_two.inc(1.0, None);
             fast_gauge.set(x as f64, None);
             random_gauge.set(rand::random(), None);
             thread::sleep(Duration::from_millis(100));
         }
         gauge.set(10.0, None);
-        counter.inc(1.0, None);
+        counter_two.inc(1.0, None);
+        counter_one.inc(1.0, None);
         logger.log(format!("okay :)"), None);
     }
 
