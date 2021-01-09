cb56bc5d9830592e9893cff8fd926ca97f92de6c
overfloat <rillrate@gmail.com>
2021-01-09 12:03:33 +0300

    Add fast logger

--- rill-export/examples/all.rs
+++ rill-export/examples/all.rs
@@ -26,12 +26,16 @@ fn main() -> Result<(), Error> {
     let fast_gauge = GaugeProvider::new("my.gauge.fast".parse()?);
     let random_gauge = GaugeProvider::new("my.gauge.random".parse()?);
     let logger = LogProvider::new("my.direct.logs.trace".parse()?);
+    let fast_logger = LogProvider::new("my.direct.logs.fast".parse()?);
+    let mut counter = 0;
     while running.load(Ordering::SeqCst) {
+        counter += 1;
         for x in 0..3 {
             counter_two.inc(1.0, None);
             fast_gauge.set(x as f64, None);
             random_gauge.set(rand::random(), None);
             thread::sleep(Duration::from_millis(100));
+            fast_logger.log(format!("first loop - {}/{}", counter, x), None);
         }
         gauge.set(1.0, None);
         for x in 0..7 {
@@ -39,11 +43,12 @@ fn main() -> Result<(), Error> {
             fast_gauge.set(x as f64, None);
             random_gauge.set(rand::random(), None);
             thread::sleep(Duration::from_millis(100));
+            fast_logger.log(format!("second loop - {}/{}", counter, x), None);
         }
         gauge.set(10.0, None);
         counter_two.inc(1.0, None);
         counter_one.inc(1.0, None);
-        logger.log(format!("okay :)"), None);
+        logger.log(format!("okay :) - {}", counter), None);
     }
 
     Ok(())
