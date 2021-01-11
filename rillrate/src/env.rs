7fdb2d2675b2213f16f74d1c644b34b3a55bd4aa
overfloat <rillrate@gmail.com>
2021-01-11 22:10:35 +0300

    Take RILL_NODE parameter into account

--- rillrate/src/env.rs
+++ rillrate/src/env.rs
@@ -1,4 +1,5 @@
 use std::env::{current_exe, var};
+use std::net::SocketAddr;
 use std::path::PathBuf;
 
 pub fn config() -> PathBuf {
@@ -8,11 +9,22 @@ pub fn config() -> PathBuf {
 }
 
 pub fn name() -> Option<String> {
-    var("RILL_AS").ok().or_else(|| {
+    var("RILL_NAME").ok().or_else(|| {
         current_exe().ok().and_then(|buf| {
             buf.as_path()
                 .file_name()
                 .and_then(|path| path.to_str().map(String::from))
         })
+    })
+}
+
+pub fn node() -> Option<SocketAddr> {
+    var("RILL_NODE").ok().and_then(|s| {
+        s.parse::<SocketAddr>()
+            .map_err(|err| {
+                // TODO: Return error here
+                log::error!("Can't parse address: {}", err);
+            })
+            .ok()
     })
 }
--- rillrate/src/lib.rs
+++ rillrate/src/lib.rs
@@ -11,7 +11,7 @@ use rill_export::RillExport;
 /// `RillRate` tracer.
 #[derive(Debug)]
 pub struct RillRate {
-    _rill_export: RillExport,
+    _rill_export: Option<RillExport>,
     _rill: Rill,
 }
 
@@ -19,7 +19,10 @@ impl RillRate {
     /// Create a new instance configured by env vars.
     pub fn from_env(default_name: &str) -> Result<Self, Error> {
         let config_path = Some(env::config());
-        let _rill_export = RillExport::start(config_path)?;
+        let mut _rill_export = None;
+        if env::node().is_none() {
+            _rill_export = Some(RillExport::start(config_path)?);
+        }
         let name = env::name().unwrap_or_else(|| default_name.into());
         let _rill = Rill::install(name)?;
         Ok(Self {
