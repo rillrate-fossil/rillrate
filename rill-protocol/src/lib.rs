bbb61e85bf60f9fc5b0197a5ccaeef71f1202f15
overfloat <rillrate@gmail.com>
2021-03-12 13:19:54 +0300

    Add improved encoding module

--- rill-protocol/src/lib.rs
+++ rill-protocol/src/lib.rs
@@ -4,6 +4,23 @@ pub mod frame;
 pub mod io;
 pub mod pathfinder;
 
-pub use serde_json as encoding;
+mod encoding {
+    use anyhow::Error;
+    use serde::{Deserialize, Serialize};
+
+    pub fn from_slice<'a, T>(v: &'a [u8]) -> Result<T, Error>
+    where
+        T: Deserialize<'a>,
+    {
+        serde_json::from_slice(v).map_err(Error::from)
+    }
+
+    pub fn to_vec<T: ?Sized>(value: &T) -> Result<Vec<u8>, Error>
+    where
+        T: Serialize,
+    {
+        serde_json::to_vec(value).map_err(Error::from)
+    }
+}
 
 metacrate::meta!();
