92e4a77d2b85f816aa55fa933fc864cb1a8765fa
overfloat <rillrate@gmail.com>
2021-03-18 14:01:10 +0300

    Use PulseMetric inside GaugeTracer

--- rill-engine/src/tracers/data/gauge.rs
+++ rill-engine/src/tracers/data/gauge.rs
@@ -1,45 +1,28 @@
 use crate::tracers::tracer::Tracer;
 use derive_more::{Deref, DerefMut};
-use rill_protocol::data::gauge::{GaugeEvent, GaugeMetric, GaugeState};
+use rill_protocol::data::pulse::{PulseEvent, PulseMetric, PulseState};
 use rill_protocol::io::provider::Path;
+use rill_protocol::range::Range;
 use std::time::SystemTime;
 
 /// Tracers `Gauge` metrics.
 #[derive(Debug, Deref, DerefMut, Clone)]
 pub struct GaugeTracer {
-    #[deref]
-    #[deref_mut]
-    tracer: Tracer<GaugeMetric>,
-    min: f64,
-    max: f64,
+    tracer: Tracer<PulseMetric>,
 }
 
 impl GaugeTracer {
     /// Creates a new tracer instance.
-    pub fn new(path: Path, mut min: f64, mut max: f64) -> Self {
-        if min > max {
-            std::mem::swap(&mut min, &mut max);
-        }
-        if (min - max).abs() < f64::EPSILON {
-            max = min + 1.0;
-        }
-        let state = GaugeState::new(min, max);
+    pub fn new(path: Path, min: f64, max: f64) -> Self {
+        let range = Range::new(min, max);
+        let state = PulseState::new(Some(range), None);
         let tracer = Tracer::new(state, path, None);
-        Self { tracer, min, max }
+        Self { tracer }
     }
 
     /// Set value of the gauge.
     pub fn set(&self, value: f64, timestamp: Option<SystemTime>) {
-        let value = {
-            if value < self.min {
-                self.min
-            } else if value > self.max {
-                self.max
-            } else {
-                value
-            }
-        };
-        let data = GaugeEvent::Set(value);
+        let data = PulseEvent::Set(value);
         self.tracer.send(data, timestamp);
     }
 }
