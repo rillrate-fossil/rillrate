73fd52add2503c497c3e843676575857397fb290
overfloat <rillrate@gmail.com>
2021-03-19 22:52:36 +0300

    Use proper metric objects for Counter and Gauge tracers

--- rill-engine/src/tracers/data/counter.rs
+++ rill-engine/src/tracers/data/counter.rs
@@ -1,27 +1,27 @@
 use crate::tracers::tracer::Tracer;
 use derive_more::{Deref, DerefMut};
-use rill_protocol::data::pulse::{PulseEvent, PulseMetric, PulseState};
+use rill_protocol::data::counter::{CounterEvent, CounterMetric, CounterState};
 use rill_protocol::io::provider::Path;
 use std::time::SystemTime;
 
 /// Tracers `Counter` metrics that can increments only.
 #[derive(Debug, Deref, DerefMut, Clone)]
 pub struct CounterTracer {
-    tracer: Tracer<PulseMetric>,
+    tracer: Tracer<CounterMetric>,
 }
 
 impl CounterTracer {
     /// Creates a new tracer instance.
     pub fn new(path: Path) -> Self {
-        let metric = PulseMetric::Counter;
-        let state = PulseState::new(None);
+        let metric = CounterMetric;
+        let state = CounterState::new();
         let tracer = Tracer::new(metric, state, path, None);
         Self { tracer }
     }
 
     /// Increments value by the sepcific delta.
     pub fn inc(&self, delta: f64, timestamp: Option<SystemTime>) {
-        let data = PulseEvent::Inc(delta);
+        let data = CounterEvent::Inc(delta);
         self.tracer.send(data, timestamp);
     }
 }
--- rill-engine/src/tracers/data/gauge.rs
+++ rill-engine/src/tracers/data/gauge.rs
@@ -1,6 +1,6 @@
 use crate::tracers::tracer::Tracer;
 use derive_more::{Deref, DerefMut};
-use rill_protocol::data::pulse::{PulseEvent, PulseMetric, PulseState};
+use rill_protocol::data::gauge::{GaugeEvent, GaugeMetric, GaugeState};
 use rill_protocol::io::provider::Path;
 use rill_protocol::range::Range;
 use std::time::SystemTime;
@@ -8,22 +8,22 @@ use std::time::SystemTime;
 /// Tracers `Gauge` metrics.
 #[derive(Debug, Deref, DerefMut, Clone)]
 pub struct GaugeTracer {
-    tracer: Tracer<PulseMetric>,
+    tracer: Tracer<GaugeMetric>,
 }
 
 impl GaugeTracer {
     /// Creates a new tracer instance.
     pub fn new(path: Path, min: f64, max: f64) -> Self {
         let range = Range::new(min, max);
-        let metric = PulseMetric::Gauge { range };
-        let state = PulseState::new(None);
+        let metric = GaugeMetric { range };
+        let state = GaugeState::new();
         let tracer = Tracer::new(metric, state, path, None);
         Self { tracer }
     }
 
     /// Set value of the gauge.
     pub fn set(&self, value: f64, timestamp: Option<SystemTime>) {
-        let data = PulseEvent::Set(value);
+        let data = GaugeEvent::Set(value);
         self.tracer.send(data, timestamp);
     }
 }
