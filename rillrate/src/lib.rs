e70312fb9c23f7a94c5b3f7e7e4e2d00f84fdd05
overfloat <rillrate@gmail.com>
2021-03-21 19:15:27 +0300

    Add install and uninstall methods

--- rillrate/src/lib.rs
+++ rillrate/src/lib.rs
@@ -14,6 +14,10 @@ pub use tracers::*;
 
 use anyhow::Error;
 use meio::thread::ScopedRuntime;
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+static INSTALLED: Lazy<Mutex<Option<RillRate>>> = Lazy::new(|| Mutex::new(None));
 
 /// The tracer.
 pub struct RillRate {
@@ -27,5 +31,26 @@ impl RillRate {
         let actor = RillRate::new(app_name.to_string());
         let _scoped = meio::thread::spawn(actor)?;
         Ok(Self { _scoped })
+    }
+
+    /// Create and install an instance of `RillRate` into the global cell.
+    /// The provider will be alive and available until the `uninstall` method call.
+    pub fn install(app_name: impl ToString) -> Result<(), Error> {
+        let instance = Self::from_env(app_name)?;
+        // TODO: Metod like `Option::swap` instad?
+        let mut cell = INSTALLED
+            .lock()
+            .map_err(|err| Error::msg(err.to_string()))?;
+        *cell = Some(instance);
+        Ok(())
+    }
+
+    /// Uninstalling of the installed `RillRate` instance.
+    pub fn uninstall() -> Result<(), Error> {
+        INSTALLED
+            .lock()
+            .map_err(|err| Error::msg(err.to_string()))?
+            .take();
+        Ok(())
     }
 }
