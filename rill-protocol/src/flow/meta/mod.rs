9e7668078a161bdc1456db62192da7351845a378
overfloat <rillrate@gmail.com>
2021-03-24 14:50:30 +0300

    Add PathFlow

--- rill-protocol/src/flow/meta/mod.rs
+++ rill-protocol/src/flow/meta/mod.rs
@@ -1,7 +1,9 @@
 pub mod entry;
+pub mod path;
 
 use crate::io::provider::EntryId;
 
 pub trait MetaFlow: Default {
+    // TODO: Use `Path` here?
     fn location() -> EntryId;
 }
---
+++ rill-protocol/src/flow/meta/path.rs
@@ -0,0 +1,56 @@
+use super::MetaFlow;
+use crate::flow::data::{Flow, TimedEvent};
+use crate::io::provider::{EntryId, Path, StreamType};
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
+pub struct PathFlow;
+
+impl MetaFlow for PathFlow {
+    fn location() -> EntryId {
+        "meta:paths".into()
+    }
+}
+
+impl Flow for PathFlow {
+    type State = PathState;
+    type Event = PathEvent;
+
+    fn stream_type() -> StreamType {
+        StreamType::from("rillrate.path.v0")
+    }
+
+    fn apply(&self, state: &mut Self::State, event: TimedEvent<Self::Event>) {
+        match event.event {
+            PathEvent::AddPath { name } => {
+                state.entries.insert(name, ());
+            }
+            PathEvent::RemovePath { name } => {
+                state.entries.remove(&name);
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PathState {
+    pub entries: BTreeMap<Path, ()>,
+}
+
+#[allow(clippy::new_without_default)]
+impl PathState {
+    pub fn new() -> Self {
+        Self {
+            entries: BTreeMap::new(),
+        }
+    }
+}
+
+pub type PathDelta = Vec<TimedEvent<PathEvent>>;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum PathEvent {
+    AddPath { name: Path },
+    RemovePath { name: Path },
+}
