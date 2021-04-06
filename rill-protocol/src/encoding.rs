a7ab2b10a3fd6560f69430d3978ec8b41c141297
overfloat <rillrate@gmail.com>
2021-04-06 10:24:36 +0300

    Move encoding module to the separate file

---
+++ rill-protocol/src/encoding.rs
@@ -0,0 +1,20 @@
+use anyhow::Error;
+use serde::{Deserialize, Serialize};
+
+pub fn from_slice<'a, T>(v: &'a [u8]) -> Result<T, Error>
+where
+    T: Deserialize<'a>,
+{
+    //bincode::deserialize(v).map_err(Error::from)
+    flexbuffers::from_slice(v).map_err(Error::from)
+    //serde_json::from_slice(v).map_err(Error::from)
+}
+
+pub fn to_vec<T: ?Sized>(value: &T) -> Result<Vec<u8>, Error>
+where
+    T: Serialize,
+{
+    //bincode::serialize(value).map_err(Error::from)
+    flexbuffers::to_vec(value).map_err(Error::from)
+    //serde_json::to_vec(value).map_err(Error::from)
+}
--- rill-protocol/src/lib.rs
+++ rill-protocol/src/lib.rs
@@ -1,32 +1,10 @@
 pub mod calc;
 pub mod config;
+pub mod encoding;
 pub mod flow;
 pub mod frame;
 pub mod io;
 pub mod pathfinder;
 pub mod range;
-
-pub mod encoding {
-    use anyhow::Error;
-    use serde::{Deserialize, Serialize};
-
-    pub fn from_slice<'a, T>(v: &'a [u8]) -> Result<T, Error>
-    where
-        T: Deserialize<'a>,
-    {
-        //bincode::deserialize(v).map_err(Error::from)
-        flexbuffers::from_slice(v).map_err(Error::from)
-        //serde_json::from_slice(v).map_err(Error::from)
-    }
-
-    pub fn to_vec<T: ?Sized>(value: &T) -> Result<Vec<u8>, Error>
-    where
-        T: Serialize,
-    {
-        //bincode::serialize(value).map_err(Error::from)
-        flexbuffers::to_vec(value).map_err(Error::from)
-        //serde_json::to_vec(value).map_err(Error::from)
-    }
-}
 
 metacrate::meta!();
