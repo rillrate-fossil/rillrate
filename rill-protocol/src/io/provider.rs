27b5e563d44f97eb31b0ac02b52bfa18121d6471
overfloat <rillrate@gmail.com>
2021-04-09 18:20:11 +0300

    Add packed macros

--- rill-protocol/src/io/provider.rs
+++ rill-protocol/src/io/provider.rs
@@ -354,6 +354,28 @@ impl Description {
     }
 }
 
+macro_rules! packed {
+    ($name:ident) => {
+        #[derive(Clone, From, Into, Serialize, Deserialize, PartialEq, Eq)]
+        pub struct $name(pub Vec<u8>);
+
+        impl fmt::Debug for $name {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.debug_struct(stringify!($name))
+                    .field("size", &self.0.len())
+                    .finish()
+            }
+        }
+
+        impl AsRef<[u8]> for $name {
+            fn as_ref(&self) -> &[u8] {
+                self.0.as_ref()
+            }
+        }
+
+    };
+}
+
 #[derive(Clone, From, Into, Serialize, Deserialize, PartialEq, Eq)]
 pub struct PackedFlow(pub Vec<u8>);
 
