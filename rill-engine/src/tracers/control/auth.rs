0a57debe119e73bb509aa961b73f28448d208b75
overfloat <rillrate@gmail.com>
2021-04-20 13:16:52 +0300

    Return events for watch_auth

--- rill-engine/src/tracers/control/auth.rs
+++ rill-engine/src/tracers/control/auth.rs
@@ -1,7 +1,8 @@
 use crate::tracers::tracer::Tracer;
 use anyhow::Error;
 use derive_more::{Deref, DerefMut};
-use rill_protocol::flow::control::auth::AuthState;
+use rill_protocol::flow::control::auth::{AuthEvent, AuthState};
+use rill_protocol::flow::core::TimedEvent;
 use rill_protocol::io::provider::Path;
 
 /// Receives auths from a user.
@@ -19,7 +20,7 @@ impl AuthWatcher {
     }
 
     /// Wait for the auth event.
-    pub async fn watch_auth(&mut self) -> Result<(), Error> {
-        self.tracer.recv().await.map(drop)
+    pub async fn watch_auth(&mut self) -> Result<AuthEvent, Error> {
+        self.tracer.recv().await.map(TimedEvent::into_inner)
     }
 }
