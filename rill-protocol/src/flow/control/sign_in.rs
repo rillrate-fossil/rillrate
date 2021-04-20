38f5445563fc4fb2759761eccfd0970d4c7d0c6d
overfloat <rillrate@gmail.com>
2021-04-20 11:20:14 +0300

    Add SignIn flow

---
+++ rill-protocol/src/flow/control/sign_in.rs
@@ -0,0 +1,55 @@
+use crate::flow::core::{Flow, TimedEvent};
+use crate::io::provider::{StreamType, Timestamp};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum AuthState {
+    Unauthorized,
+    LoggingIn,
+    Authorized,
+    LoggingOut,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SignInState {
+    pub auth_state: AuthState,
+    pub last_change: Option<Timestamp>,
+}
+
+impl SignInState {
+    pub fn new() -> Self {
+        Self {
+            auth_state: AuthState::Unauthorized,
+            last_change: None,
+        }
+    }
+}
+
+impl Flow for SignInState {
+    type Event = SignInEvent;
+
+    fn stream_type() -> StreamType {
+        StreamType::from("rillrate.flow.control.signin.v0")
+    }
+
+    fn apply(&mut self, event: TimedEvent<Self::Event>) {
+        match event.event {
+            SignInEvent::TrySignIn { .. } => {
+                self.auth_state = AuthState::LoggingIn;
+            }
+            SignInEvent::TrySignOut => {
+                self.auth_state = AuthState::LoggingOut;
+            }
+        }
+        self.last_change = Some(event.timestamp);
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum SignInEvent {
+    TrySignIn {
+        username: String,
+        password: String,
+    },
+    TrySignOut,
+}
