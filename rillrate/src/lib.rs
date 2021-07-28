34df4072116c383200f47027b2c963931bdcbd02
Denis Kolodin <deniskolodin@gmail.com>
2021-07-28 10:02:24 +0300

    Add RillRateHandle

--- rillrate/src/lib.rs
+++ rillrate/src/lib.rs
@@ -1,1 +1,15 @@
 pub mod actors;
+
+use actors::supervisor::NodeSupervisor;
+use anyhow::Error;
+use meio::thread;
+
+pub struct RillRateHandle {
+    rt: thread::ScopedRuntime,
+}
+
+pub fn start_engine() -> Result<RillRateHandle, Error> {
+    let actor = NodeSupervisor::new(Default::default());
+    let rt = thread::spawn(actor)?;
+    Ok(RillRateHandle { rt })
+}
