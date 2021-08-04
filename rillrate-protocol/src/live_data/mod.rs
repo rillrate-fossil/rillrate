96596316542d166c951de94e9145736de9d5c9b3
Denis Kolodin <deniskolodin@gmail.com>
2021-08-04 10:24:40 +0300

    Hide binder with the engine feature

--- rillrate-protocol/src/live_data/mod.rs
+++ rillrate-protocol/src/live_data/mod.rs
@@ -1,7 +1,9 @@
 //! Live data streams tracks time automatically and used to provide live data.
 
 pub mod counter;
+#[cfg(feature = "engine")]
 pub use counter::CounterStatTracer;
 
 pub mod pulse;
+#[cfg(feature = "engine")]
 pub use pulse::PulseFrameTracer;
--- rillrate-protocol/src/manifest/mod.rs
+++ rillrate-protocol/src/manifest/mod.rs
@@ -1,4 +1,6 @@
+#[cfg(feature = "engine")]
 pub mod binder;
+#[cfg(feature = "engine")]
 pub use binder::Binded;
 
 pub mod descriptions_list;
