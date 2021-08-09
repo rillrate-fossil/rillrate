f54891044bbf1c6a32920b8afcf48ee83538f0e5
Denis Kolodin <deniskolodin@gmail.com>
2021-08-09 11:27:34 +0300

    Add range to the spec

--- rillrate-protocol/src/live_data/pulse/state.rs
+++ rillrate-protocol/src/live_data/pulse/state.rs
@@ -3,15 +3,37 @@ use serde::{Deserialize, Serialize};
 
 pub type PulseFrameState = FrameFlowState<PulseFrameSpec>;
 
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct Range {
+    pub min: Option<f32>,
+    pub max: Option<f32>,
+}
+
+impl Range {
+    pub fn new(mut min: f32, mut max: f32) -> Self {
+        if min > max {
+            std::mem::swap(&mut min, &mut max);
+        }
+        Self {
+            min: Some(min),
+            max: Some(max),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PulseFrameSpec {
     /// Retain interval in seconds.
     pub retain: u32,
+    pub range: Range,
 }
 
 impl Default for PulseFrameSpec {
     fn default() -> Self {
-        Self { retain: 30 }
+        Self {
+            retain: 30,
+            range: Range::default(),
+        }
     }
 }
 
