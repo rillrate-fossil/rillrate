acd1261df12bd26511bb6481c09d015a7245ea0f
Denis Kolodin <deniskolodin@gmail.com>
2021-08-14 11:33:37 +0300

    Keep the handle of the engine globally

--- rillrate/src/lib.rs
+++ rillrate/src/lib.rs
@@ -12,13 +12,25 @@ pub use rillrate_protocol::Link;
 use actors::supervisor::NodeSupervisor;
 use anyhow::Error;
 use meio::thread;
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+static GLOBAL: Lazy<Mutex<Option<RillRateHandle>>> = Lazy::new(|| Mutex::new(None));
 
 pub struct RillRateHandle {
     _rt: thread::ScopedRuntime,
 }
 
-pub fn start() -> Result<RillRateHandle, Error> {
+pub fn start() -> Result<(), Error> {
     let actor = NodeSupervisor::new(Default::default());
     let rt = thread::spawn(actor)?;
-    Ok(RillRateHandle { _rt: rt })
+    let mut opt_handle = GLOBAL.lock().map_err(|err| Error::msg(err.to_string()))?;
+    *opt_handle = Some(RillRateHandle { _rt: rt });
+    Ok(())
+}
+
+pub fn stop() -> Result<(), Error> {
+    let mut opt_handle = GLOBAL.lock().map_err(|err| Error::msg(err.to_string()))?;
+    opt_handle.take();
+    Ok(())
 }
