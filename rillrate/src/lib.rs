c74d08a3164ce8428b855c85ae77b26736c3df08
Denis Kolodin <deniskolodin@gmail.com>
2021-08-14 12:00:59 +0300

    Add docs to the rillrate crate

--- rillrate/src/lib.rs
+++ rillrate/src/lib.rs
@@ -1,4 +1,8 @@
-pub mod actors;
+//! `RillRate` - Dynamic UI for bots, microservices, and IoT.
+
+#![warn(missing_docs)]
+
+mod actors;
 
 metacrate::meta!();
 
@@ -6,8 +10,6 @@ pub use rillrate_protocol::live_control::*;
 pub use rillrate_protocol::live_flow::*;
 pub use rillrate_protocol::range;
 pub use rillrate_protocol::Link;
-
-// TODO: Add wrappers for tracers?
 
 use actors::supervisor::NodeSupervisor;
 use anyhow::Error;
@@ -17,10 +19,12 @@ use std::sync::Mutex;
 
 static GLOBAL: Lazy<Mutex<Option<RillRateHandle>>> = Lazy::new(|| Mutex::new(None));
 
+/// Tracks a lifetime of the `RillRate` engine.
 pub struct RillRateHandle {
     _rt: thread::ScopedRuntime,
 }
 
+/// Starts the engine.
 pub fn start() -> Result<(), Error> {
     let actor = NodeSupervisor::new(Default::default());
     let rt = thread::spawn(actor)?;
@@ -29,6 +33,7 @@ pub fn start() -> Result<(), Error> {
     Ok(())
 }
 
+/// Stops the engine.
 pub fn stop() -> Result<(), Error> {
     let mut opt_handle = GLOBAL.lock().map_err(|err| Error::msg(err.to_string()))?;
     opt_handle.take();
