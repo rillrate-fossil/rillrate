e5c3f537b3dec9dbb4230a956edaf5b3c1e84f7b
Denis Kolodin <deniskolodin@gmail.com>
2021-08-22 10:55:16 +0300

    Implement Board explicitly

--- rrpack-prime/src/live_flow/board/state.rs
+++ rrpack-prime/src/live_flow/board/state.rs
@@ -1,18 +1,47 @@
-use crate::base_flow::list_flow::{ListFlowSpec, ListFlowState};
+use rill_protocol::flow::core::Flow;
+use rill_protocol::io::provider::StreamType;
 use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BoardSpec;
 
-impl ListFlowSpec for BoardSpec {
-    type Id = String;
-    type Record = String;
-    type Action = ();
-    type Update = ();
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BoardState {
+    pub map: BTreeMap<String, String>,
+}
 
-    fn update_record(_record: &mut Self::Record, _update: Self::Update) {
-        log::error!("Inner updates are not supported for Board");
+#[allow(clippy::new_without_default)]
+impl BoardState {
+    pub fn new() -> Self {
+        Self {
+            map: BTreeMap::new(),
+        }
     }
 }
 
-pub type BoardState = ListFlowState<BoardSpec>;
+impl Flow for BoardState {
+    type Action = ();
+    type Event = BoardEvent;
+
+    fn stream_type() -> StreamType {
+        StreamType::from(module_path!())
+    }
+
+    fn apply(&mut self, event: Self::Event) {
+        match event {
+            BoardEvent::Assign { key, value } => {
+                self.map.insert(key, value);
+            }
+            BoardEvent::Remove { key } => {
+                self.map.remove(&key);
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BoardEvent {
+    Assign { key: String, value: String },
+    Remove { key: String },
+}
--- rrpack-prime/src/live_flow/board/tracer.rs
+++ rrpack-prime/src/live_flow/board/tracer.rs
@@ -1,20 +1,41 @@
 use super::state::*;
 use crate::auto_path::AutoPath;
-use crate::base_flow::list_flow::ListFlowTracer;
-use crate::manifest::Binded;
+use crate::manifest::Binder;
+use derive_more::{Deref, DerefMut};
+use rill_engine::tracers::tracer::Tracer;
 
+#[derive(Debug, Deref, DerefMut, Clone)]
 pub struct Board {
-    tracer: Binded<ListFlowTracer<BoardSpec>>,
+    #[deref]
+    #[deref_mut]
+    tracer: Tracer<BoardState>,
+    _binder: Binder,
 }
 
 impl Board {
     pub fn new(auto_path: impl Into<AutoPath>) -> Self {
         let path = auto_path.into();
-        let tracer = Binded::new(ListFlowTracer::new(path.into()));
-        Self { tracer }
+        let state = BoardState::new();
+        let tracer = Tracer::new(state, path.into(), None);
+        let binder = Binder::new(&tracer);
+        Self {
+            tracer,
+            _binder: binder,
+        }
     }
 
     pub fn set(&self, key: impl ToString, value: impl ToString) {
-        self.tracer.add_record(key.to_string(), value.to_string());
+        let msg = BoardEvent::Assign {
+            key: key.to_string(),
+            value: value.to_string(),
+        };
+        self.tracer.send(msg, None);
+    }
+
+    pub fn remove(&self, key: impl ToString) {
+        let msg = BoardEvent::Remove {
+            key: key.to_string(),
+        };
+        self.tracer.send(msg, None);
     }
 }
