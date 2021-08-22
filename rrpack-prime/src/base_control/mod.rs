fbf9bae487eadfbcd1407d5cd18030f6d59691f5
Denis Kolodin <deniskolodin@gmail.com>
2021-08-22 11:04:21 +0300

    Remove base_control module

--- rrpack-prime/src/base_control/mod.rs
+++
@@ -1,1 +0,0 @@
-//pub mod emit_control;
--- rrpack-prime/src/lib.rs
+++ rrpack-prime/src/lib.rs
@@ -1,7 +1,5 @@
 //#![warn(missing_docs)]
 
-// TODO: Maybe this module should be moved to the `rill-protocol`?
-pub mod base_control;
 pub mod base_flow;
 pub mod live_control;
 pub mod live_flow;
