8cd24152fea0edf2b4e5e1f3b96d2ae94b17b275
Denis Kolodin <deniskolodin@gmail.com>
2021-08-25 22:52:37 +0300

    Sort prime tracers by layers

--- rillrate/src/prime.rs
+++ rillrate/src/prime.rs
@@ -1,5 +1,6 @@
 //! The prime pack: board, pulse, table, histogram, click, selector, etc.
 
-pub use rrpack_prime::live_control::*;
-pub use rrpack_prime::live_flow::*;
+pub use rrpack_prime::control::*;
 pub use rrpack_prime::range;
+pub use rrpack_prime::transparent::*;
+pub use rrpack_prime::visual::*;
--- rrpack-prime/src/lib.rs
+++ rrpack-prime/src/lib.rs
@@ -1,9 +1,10 @@
 //#![warn(missing_docs)]
 
-pub mod live_control;
-pub mod live_flow;
-pub mod manifest;
+pub mod control;
+pub mod transparent;
+pub mod visual;
 
 pub mod auto_path;
+pub mod manifest;
 pub mod range;
 mod utils;
