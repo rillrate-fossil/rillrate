1936d27fa46f44d6f5462f97b16abdc3c5082688
Denis Kolodin <deniskolodin@gmail.com>
2021-09-07 22:41:02 +0300

    Add separate LayoutConfig struct

--- packs/prime/src/manifest/layouts/layout.rs
+++ packs/prime/src/manifest/layouts/layout.rs
@@ -1,15 +1,76 @@
 use crate::auto_path::AutoPath;
+use rill_protocol::io::provider::{EntryId, Path};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub struct Layout {
+pub struct LayoutConfig {
     pub name: String,
-    pub item: Option<Vec<LayoutItem>>,
+    pub item: Option<Vec<LayoutItemConfig>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct LayoutItemConfig {
+    // TODO: Use `Position` in non-config
+    pub position: (u32, u32),
+    pub size: (u32, u32),
+    pub path: AutoPath,
+}
+
+impl From<LayoutConfig> for Layout {
+    fn from(config: LayoutConfig) -> Self {
+        let items = config
+            .item
+            .unwrap_or_default()
+            .into_iter()
+            .map(LayoutItem::from)
+            .collect();
+        Self {
+            name: config.name.into(),
+            items,
+        }
+    }
+}
+
+impl From<LayoutItemConfig> for LayoutItem {
+    fn from(config: LayoutItemConfig) -> Self {
+        let position = Position {
+            left: config.position.0,
+            top: config.position.1,
+        };
+        let size = Size {
+            width: config.size.0,
+            height: config.size.1,
+        };
+        let path = config.path.into();
+        Self {
+            position,
+            size,
+            path,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Layout {
+    pub name: EntryId,
+    pub items: Vec<LayoutItem>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct LayoutItem {
-    pub position: (u32, u32),
-    pub size: (u32, u32),
-    pub path: AutoPath,
+    pub position: Position,
+    pub size: Size,
+    pub path: Path,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Position {
+    pub left: u32,
+    pub top: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Size {
+    pub width: u32,
+    pub height: u32,
 }
