c2e4ba85ba52da3b700c7b5cb30cd9f13653dd46
Denis Kolodin <deniskolodin@gmail.com>
2021-09-09 23:06:37 +0300

    Fix warnings

--- rillrate/src/actors/config_watcher/actor.rs
+++ rillrate/src/actors/config_watcher/actor.rs
@@ -2,11 +2,11 @@ use crate::actors::supervisor::NodeSupervisor;
 use crate::config::RillRateConfig;
 use anyhow::Error;
 use async_trait::async_trait;
-use meio::task::{HeartBeat, OnTick, Tick};
+use meio::task::{OnTick, Tick};
 use meio::{Actor, Context, InterruptedBy, StartedBy};
 use rate_config::ReadableConfig;
 use rrpack_prime::manifest::layouts::global::LAYOUTS;
-use tokio::time::Duration;
+
 
 pub struct ConfigWatcher {}
 
@@ -22,7 +22,7 @@ impl Actor for ConfigWatcher {
 
 #[async_trait]
 impl StartedBy<NodeSupervisor> for ConfigWatcher {
-    async fn handle(&mut self, ctx: &mut Context<Self>) -> Result<(), Error> {
+    async fn handle(&mut self, _ctx: &mut Context<Self>) -> Result<(), Error> {
         /* TODO: Implement live updates
         let interval = Duration::from_secs(5);
         let _heartbeat = HeartBeat::new(interval, ctx.address().clone());
@@ -61,7 +61,7 @@ impl ConfigWatcher {
 
 #[async_trait]
 impl OnTick for ConfigWatcher {
-    async fn tick(&mut self, _: Tick, ctx: &mut Context<Self>) -> Result<(), Error> {
+    async fn tick(&mut self, _: Tick, _ctx: &mut Context<Self>) -> Result<(), Error> {
         self.read_config().await;
         Ok(())
     }
