b01bfb8a887e760b98ee86d125e570308b015c55
Denis Kolodin <deniskolodin@gmail.com>
2021-09-22 23:45:05 +0300

    Add embed_config macro

--- pkg-core/rate-config/src/preserved.rs
+++ pkg-core/rate-config/src/preserved.rs
@@ -1,3 +1,11 @@
 use once_cell::sync::OnceCell;
 
 pub static PRESERVED: OnceCell<Vec<u8>> = OnceCell::new();
+
+#[macro_export]
+macro_rules! embed_config {
+    () => {
+        let value = include_bytes!(env!("RR_CONFIG"));
+        $crate::preserved::PRESERVED.set(value.to_vec());
+    };
+}
