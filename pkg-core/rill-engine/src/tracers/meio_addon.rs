c7501cbf5d7ac384aed8f20b2790174f94673872
Denis Kolodin <deniskolodin@gmail.com>
2021-09-26 00:19:23 +0300

    Add Tag to the TracerAction

--- pkg-core/rill-engine/src/tracers/meio_addon.rs
+++ pkg-core/rill-engine/src/tracers/meio_addon.rs
@@ -1,27 +1,32 @@
 //! Adds `meio` integraion to `Tracer`.
 
 use super::tracer::Tracer;
-use meio::{Action, ActionHandler, Actor, Address};
+use meio::{Action, ActionHandler, Actor, Context};
 use rill_protocol::flow::core::{ActionEnvelope, Flow};
 
 /// Tracer action for `meio` actor.
-pub struct TracerAction<T: Flow> {
+pub struct TracerAction<T: Flow, Tag> {
     /// Assigned envelope with an `Action`.
     pub envelope: ActionEnvelope<T>,
+    /// Assigned tag of the action.
+    pub tag: Tag,
 }
 
-impl<T: Flow> Action for TracerAction<T> {}
+impl<T: Flow, Tag: Send + 'static> Action for TracerAction<T, Tag> {}
 
 impl<T: Flow> Tracer<T> {
     /// Forward `Tracer` events to an `Actor`.
-    pub fn forward<A: Actor>(&self, addr: Address<A>)
+    pub fn forward<A: Actor, Tag>(&self, tag: Tag, ctx: &mut Context<A>)
     where
-        A: ActionHandler<TracerAction<T>>,
+        A: ActionHandler<TracerAction<T, Tag>>,
+        Tag: Clone + Send + Sync + 'static,
     {
+        let addr = ctx.address().clone();
         self.async_callback(move |envelope| {
             let mut addr = addr.clone();
+            let tag = tag.clone();
             async move {
-                let msg = TracerAction { envelope };
+                let msg = TracerAction { envelope, tag };
                 addr.act(msg).await
             }
         });
