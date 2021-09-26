abbeda080860007b500dd5aab8f554ec1bda4bda
Denis Kolodin <deniskolodin@gmail.com>
2021-09-26 16:51:33 +0300

    Link to explorer from empty cases

--- pkg-dashboard/rate-app/src/cases/dashboard.rs
+++ pkg-dashboard/rate-app/src/cases/dashboard.rs
@@ -1,4 +1,5 @@
 use super::state::{CasesState, CASES};
+use crate::welcome::state::{GlobalScene, SceneState, SCENE};
 use anyhow::Error;
 use rate_ui::shared_object::{DataChanged, SharedObject};
 use rate_ui::widget::{Context, NotificationHandler, Widget, WidgetRuntime};
@@ -7,36 +8,57 @@ use yew::{html, Html};
 pub type Dashboard = WidgetRuntime<DashboardWidget>;
 
 pub struct DashboardWidget {
-    scene: SharedObject<CasesState>,
+    scene: SharedObject<SceneState>,
+    cases: SharedObject<CasesState>,
 }
 
 impl Default for DashboardWidget {
     fn default() -> Self {
         Self {
-            scene: CASES.with(SharedObject::clone),
+            scene: SCENE.with(SharedObject::clone),
+            cases: CASES.with(SharedObject::clone),
         }
     }
 }
 
+#[derive(Clone)]
+pub enum Msg {
+    ToExplorer,
+}
+
 impl Widget for DashboardWidget {
-    type Event = ();
+    type Event = Msg;
     type Tag = ();
     type Properties = ();
     type Meta = ();
 
     fn init(&mut self, ctx: &mut Context<Self>) {
-        self.scene.subscribe(ctx);
+        self.cases.subscribe(ctx);
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        let state = self.scene.read();
+    fn on_event(&mut self, event: Self::Event, _ctx: &mut Context<Self>) {
+        match event {
+            Msg::ToExplorer => {
+                let mut scene = self.scene.write();
+                scene.global_scene = GlobalScene::Explorer;
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let state = self.cases.read();
         if let Some(layout) = state.get_layout_tab() {
             html! {
                 <super::LayoutViewer layout_tab=layout.clone() />
             }
         } else {
             html! {
-                <p>{ "Loading" }</p>
+                <div>
+                    <p>
+                        { "No cases available. Try to use: " }
+                        <a class="link click" onclick=ctx.event(Msg::ToExplorer)>{ "Explorer" }</a>
+                    </p>
+                </div>
             }
         }
     }
